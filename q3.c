83b2703e5c037ca0e35209f2c2d2db057d195465
Omm33 <ingaleom290@gmail.com>
2026-08-31 19:21:52 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+float radius = 1.1;
+
+float area;
+
+area = (3.14*radius*radius);
+
+printf("\n Area of Circle = %f ", area);
+
+return 0;
+
+}
---
+++ q4.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main()
+
+{
+
+float radius, area;
+
+printf("\n Enter the radius of the circle: ");
+
+scanf("%f", &radius);
+
+area = (3.14*radius*radius);
+
+printf("\n Area of Circle= %f", area);
+
+return 0;
+
+}
